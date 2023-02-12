c1491ded2fc37c98cb03c9bbcf6a90612e5494e4
Maria Patiño <maria.patinog@um.es>
2023-02-12 14:52:06 +0100

    Script shell con el que he cortado los fastq

---
+++ file-cut.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+entrada=$1
+salida=$2
+numerolinea=0
+
+cat $entrada | while read linea
+do
+	let numerolinea=$numerolinea+1
+	if test $(($numerolinea % 27)) -eq 0; then
+		echo "#$numerolinea : $linea"
+	fi
+done > $salida;
+
